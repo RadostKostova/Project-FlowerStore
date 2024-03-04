657b86dc499df3235772d981d4f525d8d7f70e8b
Radost Kostova <radostt_kostova@abv.bg>
2024-03-04 11:03:26 +0200

    Extensions for ServiceCollection added to simplify Program.cs

---
+++ FlowerStore/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using FlowerStore.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// These extension methods are used to clean up the Program.cs class. Extensions for appServices, Db context and Identity
+    /// </summary>
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}

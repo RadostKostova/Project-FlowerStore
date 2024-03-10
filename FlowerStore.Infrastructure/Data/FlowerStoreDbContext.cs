36f6c3f9f67045d39cb1a2ed1257d88f33e286d4
Radost Kostova <radostt_kostova@abv.bg>
2024-03-10 20:50:55 +0200

    Added and configured entities.

---
+++ FlowerStore.Infrastructure/Data/FlowerStoreDbContext.cs
@@ -0,0 +1,72 @@
+﻿using BookingSystem.Infrastructure.Data.Models.Roles;
+using FlowerStore.Infrastructure.Data.Models;
+using FlowerStore.Infrastructure.Data.Models.Cart;
+using FlowerStore.Infrastructure.Data.Models.Carts;
+using FlowerStore.Infrastructure.Data.Models.Categories;
+using FlowerStore.Infrastructure.Data.Models.Orders.Order;
+using FlowerStore.Infrastructure.Data.Models.Payment;
+using FlowerStore.Infrastructure.Data.Seed.EntitiesConfiguration;
+using FlowerStore.Infrastructure.Data.Seed.EntitiesConfiguration.RolesConfiguration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerStore.Infrastructure.Data
+{
+    public class FlowerStoreDbContext : IdentityDbContext
+    {
+        /// <summary>
+        /// Declaring DBSets and configuration with initial data seeding.
+        /// </summary>
+        public FlowerStoreDbContext(DbContextOptions<FlowerStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<CardDetails> CardDetails { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ProductShoppingCart> ProductShoppingCarts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //Specify precision for price properties and delete behaviour.
+            builder.Entity<Order>()
+                .Property(o => o.TotalPrice)
+                .HasPrecision(18, 2);       ////HasColumnType("decimal(18, 2)")
+
+            builder.Entity<ProductOrder>()
+                .Property(op => op.UnitPrice)
+                .HasPrecision(18, 2);
+
+            builder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            //Mapping entities (tables)
+            builder.Entity<ProductCategory>()
+                .HasKey(pc => new { pc.ProductId, pc.CategoryId });
+
+            builder.Entity<ProductShoppingCart>()
+                .HasKey(psc => new { psc.ProductId, psc.ShoppingCartId });
+
+            builder.Entity<ProductOrder>()
+                .HasKey(op => new { op.ProductId, op.OrderId });
+
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new AdministratorConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new PaymentMethodConfiguration());
+            builder.ApplyConfiguration(new OrderStatusConfiguration());
+            builder.ApplyConfiguration(new ProductConfiguration());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}

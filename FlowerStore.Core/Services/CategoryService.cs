3203f73fae41b537dd99917b0c0d51942d06df57
Radost Kostova <radostt_kostova@abv.bg>
2024-03-12 01:03:23 +0200

    Added Services

---
+++ FlowerStore.Core/Services/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using FlowerStore.Core.Contracts;
+using FlowerStore.Core.ViewModels.Category;
+using FlowerStore.Infrastructure.Common;
+using FlowerStore.Infrastructure.Data.Models;
+using FlowerStore.Infrastructure.Data.Models.Categories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerStore.Core.Services
+{
+    /// <summary>
+    /// Service methods for categories.
+    /// </summary>
+
+    public class CategoryService : ICategoryService
+    {
+        private readonly IRepository repository;
+
+        public CategoryService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<CategoryViewModel>> GetCategoriesAsync(int productId)
+        {
+            var product = await repository.AllAsReadOnly<Product>().FirstOrDefaultAsync(p => p.Id == productId);
+
+            return await repository.AllAsReadOnly<Category>()
+                .Select(c => new CategoryViewModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ FlowerStore.Core/Services/ProductService.cs
@@ -0,0 +1,86 @@
+﻿using FlowerStore.Core.Contracts;
+using FlowerStore.Core.ViewModels.Product;
+using FlowerStore.Infrastructure.Common;
+using FlowerStore.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerStore.Core.Services
+{
+    /// <summary>
+    /// Product services implemented for more code reusability.
+    /// </summary>
+    
+    public class ProductService : IProductService
+    {
+        private readonly IRepository repository;
+
+        public ProductService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+
+        public async Task<IEnumerable<ProductAllViewModel>> ShowAllProductsAsync()
+        {
+            return await repository
+                .AllAsReadOnly<Product>()
+                .Select(p => new ProductAllViewModel()
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                })
+                .ToListAsync();
+        }
+
+        public async Task<Product> ProductByIdExistAsync(int productId)
+        {
+            var productFound = await repository
+                .AllAsReadOnly<Product>()
+                .FirstOrDefaultAsync(p => p.Id == productId);
+
+            return productFound;
+        }
+
+        public async Task<int> GetProductCountAsync(int productId)
+        {
+            var product = await repository
+                .AllAsReadOnly<Product>()
+                .FirstOrDefaultAsync(p => p.Id == productId);
+
+            return product.FlowersCount;
+        }
+
+        public async Task<bool> GetAvailabilityAsync(int productId)
+        {
+            var product = await repository.AllAsReadOnly<Product>()
+                                    .FirstOrDefaultAsync(p => p.Id == productId);
+
+            return product.Availability;
+        }
+
+        public async Task<ProductDetailsViewModel> GetProductDetailsAsync(int productId)
+        {
+            var productFound = await repository.AllAsReadOnly<Product>().FirstOrDefaultAsync(p => p.Id == productId);
+
+            var product = new ProductDetailsViewModel()
+            {
+                Id = productFound.Id,
+                Name = productFound.Name,
+                Price = productFound.Price,
+                ImageUrl = productFound.ImageUrl,
+                Availability = productFound.Availability,
+                FullDescription = productFound.FullDescription,
+                FlowersCount = productFound.FlowersCount
+            };
+
+            return product;
+        }
+
+        public Task<IEnumerable<ProductAllViewModel>> SearchProductAsync(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

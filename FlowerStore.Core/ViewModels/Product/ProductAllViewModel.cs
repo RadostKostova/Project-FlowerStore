a1b8c54b3de2d3b20760bba1f406133b96858e5c
Radost Kostova <radostt_kostova@abv.bg>
2024-03-12 01:03:37 +0200

    ViewModels added

---
+++ FlowerStore.Core/ViewModels/Product/ProductAllViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FlowerStore.Core.ViewModels.Product
+{
+    /// <summary>
+    /// This ViewModel will display Catalog with products with some of the details.
+    /// </summary>
+    
+    public class ProductAllViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }   
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
---
+++ FlowerStore.Core/ViewModels/Product/ProductDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FlowerStore.Core.ViewModels.Product
+{
+    public class ProductDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool Availability { get; set; }
+        public string FullDescription { get; set; } = string.Empty;
+        public int FlowersCount { get; set; } 
+    }
+}

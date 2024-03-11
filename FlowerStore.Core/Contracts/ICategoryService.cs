68c3c38b6caa46306f8dcb70bd973857c2c34690
Radost Kostova <radostt_kostova@abv.bg>
2024-03-12 01:00:51 +0200

    Added interfaces for services

---
+++ FlowerStore.Core/Contracts/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using FlowerStore.Core.ViewModels.Category;
+
+namespace FlowerStore.Core.Contracts
+{
+    /// <summary>
+    /// Interface for Category Service.
+    /// </summary>
+
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryViewModel>> GetCategoriesAsync(int productId);
+    }
+}
---
+++ FlowerStore.Core/Contracts/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using FlowerStore.Core.ViewModels.Product;
+using FlowerStore.Infrastructure.Data.Models;
+
+namespace FlowerStore.Core.Contracts
+{
+    /// <summary>
+    /// Interface for Product Service. Implement needed methods for further operations.
+    /// </summary>
+    
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductAllViewModel>> ShowAllProductsAsync();
+        Task<Product> ProductByIdExistAsync(int productId);
+        Task<int> GetProductCountAsync(int productId);
+        Task<bool> GetAvailabilityAsync(int productId);
+        Task<ProductDetailsViewModel> GetProductDetailsAsync(int productId);
+        Task<IEnumerable<ProductAllViewModel>> SearchProductAsync(string input);
+    }
+}

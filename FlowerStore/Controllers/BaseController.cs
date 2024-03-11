2beb001a72b0b2df0b5162f7c24bf45e1dc0ca00
Radost Kostova <radostt_kostova@abv.bg>
2024-03-12 01:02:28 +0200

    Controllers added

---
+++ FlowerStore/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerStore.Controllers
+{
+    /// <summary>
+    /// Used as Base for all controllers.
+    /// </summary>
+
+    [Authorize]
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FlowerStore/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using FlowerStore.Core.Contracts;
+using FlowerStore.Core.ViewModels.Product;
+using FlowerStore.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerStore.Controllers
+{
+    /// <summary>
+    /// Manages operations related to displaying products (flowers), such as listing all flowers, displaying details of a specific flower 
+    /// and searching for flowers based on criteria like category, name, or else.
+    /// </summary>
+    
+    public class ProductController : BaseController
+    {
+        private readonly IProductService productService;
+        
+        public ProductController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Catalog()
+        {
+            var products = await productService.ShowAllProductsAsync();
+            return View(products);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int productId) // <- TODO
+        {
+            var productFound = await productService.ProductByIdExistAsync(productId);
+
+            if (productFound == null || ModelState.IsValid == false)
+            {
+                return BadRequest(); //should create Error Page later
+            }
+
+            var model = await productService.GetProductDetailsAsync(productFound.Id);
+
+            return View(model);
+        }
+    }
+}

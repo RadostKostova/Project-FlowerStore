f83e0e8647dec42c71967c4e4d329e86e2009062
Radost Kostova <radostt_kostova@abv.bg>
2024-03-13 18:45:50 +0200

    Extension for GetUserId

---
+++ FlowerStore/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace FlowerStore.Extensions
+{
+    /// <summary>
+    /// Extension for getting the user's id. 
+    /// The method will be used frequently, so it should be an extension and will reduce code duplication.
+    /// </summary>
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+        }
+    }
+}

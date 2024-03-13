c4ed3af2bcb520040f84394f9ae6b72079427552
Radost Kostova <radostt_kostova@abv.bg>
2024-03-13 18:47:21 +0200

    Custom attribute added

---
+++ FlowerStore.Infrastructure/Attributes/DecimalRangeAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlowerStore.Components
+{
+    /// <summary>
+    /// Custom attribute for operation with prices. This will accept 2 double numbers and parse it to decimal.
+    /// </summary>
+    public class DecimalRangeAttribute : ValidationAttribute
+    {
+        public decimal MinValue { get; set; }
+        public decimal MaxValue { get; set; }
+
+        public DecimalRangeAttribute(double minValue, double maxValue)
+        {
+            MinValue = Convert.ToDecimal(minValue);
+            MaxValue = Convert.ToDecimal(maxValue);
+        }
+
+        //Handle parsing
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;  //The Required attribute will handle this
+            }
+
+            decimal decimalValue;
+
+            if (!decimal.TryParse(value.ToString(), out decimalValue))
+            {
+                return false;  //Not valid decimal
+            }
+
+            return decimalValue >= MinValue && decimalValue <= MaxValue;
+        }
+    }
+}

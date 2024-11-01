8b9e4797df792e6cd94f2334e64890568227b12d
Radost Kostova <radostt_kostova@abv.bg>
2024-11-01 02:38:17 +0200

    OrderHistory entity created

---
+++ FlowerStore.Infrastructure/Data/Models/Orders/OrderHistory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowerStore.Infrastructure.Data.Models.Orders.Order
+{
+    /// <summary>
+    /// Pernament history of orders that the User has made. 
+    /// </summary>
+
+    public class OrderHistory
+    {
+        [Key]
+        [Comment("Order history identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("User identifier")]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Order status identifier")]
+        public int OrderStatusId { get; set; }
+
+        [Required]
+        [Comment("Order identifier")]
+        public int OrderId { get; set; }
+
+        [ForeignKey(nameof(OrderStatusId))]
+        public OrderStatus OrderStatus { get; set; } = null!;
+
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; } = null!;
+    }
+}

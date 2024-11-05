cca1760c0ee6ded7b5cce18546917b0571b8d8d0
Radost Kostova <radostt_kostova@abv.bg>
2024-11-05 16:49:16 +0200

    Shaffold Identity. Changed the default Identity with extended. Added ApplicationUser with additional fields and delete empty Administrator entity

---
+++ FlowerStore.Infrastructure/Data/Models/Users/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static FlowerStore.Infrastructure.Constants.DataConstants;
+
+namespace FlowerStore.Infrastructure.Data.Models.Roles
+{
+    /// <summary>
+    /// Represents User entity and extends IdentityUser with additional fields.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(UserFirstNameMaxLength)]
+        [Comment("First name")]
+        public string? FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(UserLastNameMaxLength)]
+        [Comment("Last name")]
+        public string? LastName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(UserPhoneExactlyLength)]
+        [Comment("Phone number")]
+        public string? Phone { get; set; } = string.Empty;
+    }
+}

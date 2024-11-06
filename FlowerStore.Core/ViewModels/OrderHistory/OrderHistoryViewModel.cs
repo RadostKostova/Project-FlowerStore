259d0e302876ae020dacdcf0efa73b8b1ddf83b8
Radost Kostova <radostt_kostova@abv.bg>
2024-11-07 00:46:11 +0200

    Created OrderProductHistory entity

---
+++ FlowerStore.Core/ViewModels/OrderHistory/OrderHistoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FlowerStore.Core.ViewModels.OrderProduct;
+
+namespace FlowerStore.Core.ViewModels.OrderHistory
+{
+    public class OrderHistoryViewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string ShippingAddress { get; set; } = string.Empty;
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string OrderStatus { get; set; } = string.Empty;
+        public List<OrderProductViewModel> OrderProducts { get; internal set; } = new List<OrderProductViewModel>();
+    }
+}
+
---
+++ FlowerStore.Infrastructure/Data/Models/Orders/OrderProductHistory.cs
@@ -0,0 +1,39 @@
+﻿using FlowerStore.Infrastructure.Data.Models.Orders.Order;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowerStore.Infrastructure.Data.Models.Orders
+{
+    public class OrderProductHistory
+    {
+        [Key]
+        [Comment("Order product identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Order history identifier")]
+        public int OrderHistoryId { get; set; }
+
+        [Required]
+        [Comment("Product identifier")]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Comment("Price per unit")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Comment("Quantity of product")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Comment("Product name at the time of order")]
+        public string ProductName { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(OrderHistoryId))]
+        public OrderHistory OrderHistory { get; set; } = null!;
+    }
+}

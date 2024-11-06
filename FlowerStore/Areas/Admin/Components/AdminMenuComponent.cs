deb9db271b24f3eae335138ee83307a4fc26f322
Radost Kostova <radostt_kostova@abv.bg>
2024-11-07 00:50:26 +0200

    Created two components for the layouts with their views

---
+++ FlowerStore/Areas/Admin/Components/AdminMenuComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerStore.Areas.Admin.Components
+{
+    /// <summary>
+    /// Invoked in the admin layout
+    /// </summary>
+    
+    public class AdminMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
---
+++ FlowerStore/Components/MainMenuComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerStore.Components
+{
+    /// <summary>
+    /// Invoked in the user layout
+    /// </summary>
+    
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}

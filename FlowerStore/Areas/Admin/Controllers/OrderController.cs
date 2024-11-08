e3a5c4ca0b59a87aeb845493b75fcdef699d0901
Radost Kostova <radostt_kostova@abv.bg>
2024-11-09 01:41:12 +0200

    Created almost empty OrderController for admin area, OrderHistoryAll view is moved here

---
+++ FlowerStore/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using FlowerStore.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerStore.Areas.Admin.Controllers
+{
+    public class OrderController : AdminBaseController
+    {
+        /// <summary>
+        /// Manages operations related to orders and admin
+        /// </summary>
+        
+        private readonly IOrderService orderService;
+
+        public OrderController(IOrderService _orderService)
+        {
+            orderService = _orderService;
+        }
+        
+        //Get all orders
+        [HttpGet]
+        public async Task<IActionResult> OrderHistoryAll()
+        {
+            var allOrders = await orderService.GetAllOrdersAsync();
+            return View(allOrders);
+        }
+    }
+}

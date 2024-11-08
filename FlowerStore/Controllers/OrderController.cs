398890adf790636f8ff742965a3234e79b62e954
Radost Kostova <radostt_kostova@abv.bg>
2024-11-09 01:39:30 +0200

    Added new actions to OrderController (user part), renamed other...

--- FlowerStore/Controllers/OrderController.cs
+++ FlowerStore/Controllers/OrderController.cs
@@ -3,13 +3,13 @@ using FlowerStore.Core.ViewModels.Order;
 using FlowerStore.Core.ViewModels.OrderProduct;
 using FlowerStore.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 
 namespace FlowerStore.Controllers
 {
     /// <summary>
-    /// The OrderController handles operations related to placing an order, such as choosing payment method, 
-    /// add details to order, shipping info and etc. 
+    /// The OrderController handles operations related to placing an order, such as choosing payment method, shipping info and etc. 
     /// </summary>
 
     public class OrderController : BaseController
@@ -24,154 +24,90 @@ namespace FlowerStore.Controllers
             cartService = _cartService;
         }
 
-        //Get detailed information of order
+        //Display form for collecting user input data
         [HttpGet]
-        public async Task<IActionResult> OrderDetails(int id)
+        public async Task<IActionResult> ShippingDetails()
         {
-            var orderFound = await orderService.OrderByIdExistAsync(id);
-
-            if (orderFound == null || !ModelState.IsValid)
+            if (await cartService.IsShoppingCartEmpty(User.GetUserId()))
             {
                 return BadRequest();
             }
 
-            var model = await orderService.GetOrderDetailsAsync(orderFound.Id);
-            return View(model);
-        }
-
-        //Display form for collecting user input data
-        [HttpGet]
-        public async Task<IActionResult> ShippingDetails()  //keeps model in invalid state
-        {
             var model = new OrderFormViewModel
             {
                 PaymentMethods = await orderService.GetAllPaymentMethodsAsync()
             };
 
+            //Get previous user data input in case user clicked "Edit"
+            if (TempData.ContainsKey("OrderFormData"))
+            {
+                var previousData = JsonConvert.DeserializeObject<OrderFormViewModel>(TempData["OrderFormData"].ToString());
+                model.ShippingAddress = previousData.ShippingAddress;
+                model.OrderDetails = previousData.OrderDetails;
+                model.PaymentMethodId = previousData.PaymentMethodId;
+            }
             return View(model);
+        }
+
+        //Get user input data for further usage
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ShippingDetails(OrderFormViewModel formModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                formModel.PaymentMethods = await orderService.GetAllPaymentMethodsAsync();
+                return View(formModel);
+            }
+            return RedirectToAction(nameof(Preview), formModel);
+        }
+
+        //Get detailed information of order and create viewModel
+        [HttpGet]
+        public async Task<IActionResult> Preview(OrderFormViewModel formModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var cart = await cartService.GetOrCreateShoppingCartAsync(User.GetUserId());
+            var orderModel = await orderService.CreateOrderViewModel(formModel, cart);
+
+            if (orderModel == null)
+            {
+                return BadRequest();
+            }
+
+            //Saving temporary data if user decides to edit the form
+            TempData["OrderFormData"] = JsonConvert.SerializeObject(formModel);
+            return View(orderModel);
         }
 
         //Create new order
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ShippingDetails(OrderFormViewModel formModel)
+        public async Task<IActionResult> PlaceOrder(OrderViewModel model)
         {
-            if (formModel == null)  //add validation for empty ShippingAddress
-            {
-                return BadRequest();
-            }
-
-            var userId = User.GetUserId();
-            var cart = await cartService.GetOrCreateShoppingCartAsync(userId);
-
-            var orderModel = new OrderCreateViewModel()
-            {
-                UserId = userId,
-                ShippingAddress = formModel.ShippingAddress,
-                OrderDetails = formModel.OrderDetails,
-                PaymentMethodId = formModel.PaymentMethodId,
-                OrderDate = DateTime.Now,
-                OrderStatusId = 1,
-                ShoppingCartId = cart.Id,
-                TotalPrice = orderService.CalculateTotalPrice(cart),
-                PaymentMethods = await orderService.GetAllPaymentMethodsAsync(),
-                OrderStatuses = await orderService.GetAllOrderStatusesAsync(),
-                OrderProducts = cart.ShoppingCartProducts.Select(scp => new OrderProductViewModel
-                {
-                    OrderId = formModel.Id,
-                    ProductId = scp.ProductId,
-                    ProductName = scp.Product.Name,
-                    Price = scp.Price,
-                    Quantity = scp.Quantity
-                }).ToList()
-            };
+            //clear tempData
 
             if (!ModelState.IsValid)
             {
-                orderModel.OrderStatuses = await orderService.GetAllOrderStatusesAsync();
-                orderModel.PaymentMethods = await orderService.GetAllPaymentMethodsAsync();
-                return View(orderModel);
-            }
-            
-            var newOrderId = await orderService.CreateOrderAsync(orderModel);
-            return RedirectToAction(nameof(OrderDetails), new { id = newOrderId });
-        }
-
-        //Get edit shipping details form
-        [HttpGet]
-        public async Task<IActionResult> EditOrder(int id)
-        {
-            var orderDetails = await orderService.GetOrderDetailsAsync(id);
-            if (orderDetails == null)
-            {
-                return NotFound();
-            }
-
-            var model = new OrderEditViewModel
-            {
-                Id = orderDetails.Id,
-                ShippingAddress = orderDetails.ShippingAddress,
-                OrderDetails = orderDetails.OrderDetails,
-                PaymentMethodId = orderDetails.PaymentMethodId,
-                PaymentMethods = await orderService.GetAllPaymentMethodsAsync()
-            };
-
-            return View(model);
-        }
-
-        //Post the updated order data
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditOrder(OrderEditViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
                 model.PaymentMethods = await orderService.GetAllPaymentMethodsAsync();
-                return View(model);
+                model.OrderStatuses = await orderService.GetAllOrderStatusesAsync();
+                return View(nameof(Preview), model);
             }
 
-            var result = await orderService.UpdateOrderAsync(model);
+            var newOrderId = await orderService.CreateOrderAsync(model);
 
-            if (result == false)
+            if (newOrderId == 0)
             {
-                ModelState.AddModelError(string.Empty, "Error updating order.");
-                model.PaymentMethods = await orderService.GetAllPaymentMethodsAsync();
-                return View(model);
-            }
-            return RedirectToAction("OrderDetails", new { id = model.Id });
-        }
-
-        //Confirm the order
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Confirm(int id)
-        {
-            var userId = User.GetUserId();
-            var order = await orderService.OrderByIdExistAsync(id);
-
-            if (order == null || order.UserId != userId || order.OrderStatusId != 1)
-            {
-                return NotFound();
+                ModelState.AddModelError("", "There was an issue creating your order. Please try again.");
+                return View(nameof(Preview), model);
             }
 
-            var isConfirmed = await orderService.ConfirmOrderAsync(id);
-            if (!isConfirmed)
-            {
-                TempData["ErrorMessage"] = "Unable to confirm the order.";
-                return RedirectToAction("OrderDetails", new { id });
-            }
-
-            await cartService.ClearCartAsync(userId);
-
-            TempData["SuccessMessage"] = "Order confirmed successfully.";
-            return RedirectToAction("OrderHistoryAll");
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> OrderHistoryAll()
-        {
-            var allOrders = await orderService.GetAllOrdersAsync();
-            return View(allOrders);
+            await cartService.ClearCartAsync(User.GetUserId());
+            return RedirectToAction("OrderConfirmation", new { id = newOrderId });
         }
     }
 }

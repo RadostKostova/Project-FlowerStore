ea2c4022b1f8c1da1061bb2e48985b39072ecb17
Radost Kostova <radostt_kostova@abv.bg>
2024-11-13 22:42:14 +0200

    Created methods for cardServices what split one big task (GetOrCreateShoppingCart)

--- FlowerStore.Core/Contracts/ICartService.cs
+++ FlowerStore.Core/Contracts/ICartService.cs
@@ -1,6 +1,5 @@
 ﻿using FlowerStore.Core.ViewModels.Cart;
 using FlowerStore.Core.ViewModels.CartProduct;
-using FlowerStore.Infrastructure.Common;
 using FlowerStore.Infrastructure.Data.Models.Cart;
 
 namespace FlowerStore.Core.Contracts
@@ -11,8 +10,10 @@ namespace FlowerStore.Core.Contracts
 
     public interface ICartService
     {
-        Task<ShoppingCart> GetOrCreateShoppingCartAsync(string userId);
-        Task<CartViewModel> ViewShoppingCartAsync(string userId);
+        Task<ShoppingCart> ShoppingCartExistByUserIdAsync(string userId);
+        Task<ShoppingCart> CreateShoppingCartAsync(string userId);
+        Task<CartViewModel> GetOrCreateShoppingCartAsync(string userId);
+        Task<CartViewModel> GetShoppingCartAsync(string userId);
         Task<bool> AddProductToCartAsync(string userId, int productId, int quantity);
         Task<bool> RemoveProductFromCartAsync(string userId, int productId);
         Task<CartProductViewModel> GetProductInCartByIdAsync(int cartId, int productId);

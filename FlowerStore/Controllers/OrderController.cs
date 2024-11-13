346cf7031115d9320d1860f71fdc3613192b5e3d
Radost Kostova <radostt_kostova@abv.bg>
2024-11-13 20:36:23 +0200

    Added more actions in OrderController - CardDetails, PlaceOrder and Preview, and modified other

--- FlowerStore/Controllers/OrderController.cs
+++ FlowerStore/Controllers/OrderController.cs
@@ -1,6 +1,6 @@
 ﻿using FlowerStore.Core.Contracts;
+using FlowerStore.Core.ViewModels.CardDetails;
 using FlowerStore.Core.ViewModels.Order;
-using FlowerStore.Core.ViewModels.OrderProduct;
 using FlowerStore.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -11,7 +11,6 @@ namespace FlowerStore.Controllers
     /// <summary>
     /// The OrderController handles operations related to placing an order, such as choosing payment method, shipping info and etc. 
     /// </summary>
-
     public class OrderController : BaseController
     {
         private readonly IOrderService orderService;
@@ -24,7 +23,7 @@ namespace FlowerStore.Controllers
             cartService = _cartService;
         }
 
-        //Display form for collecting user input data
+        //Display form for shipping information
         [HttpGet]
         public async Task<IActionResult> ShippingDetails()
         {
@@ -38,18 +37,10 @@ namespace FlowerStore.Controllers
                 PaymentMethods = await orderService.GetAllPaymentMethodsAsync()
             };
 
-            //Get previous user data input in case user clicked "Edit"
-            if (TempData.ContainsKey("OrderFormData"))
-            {
-                var previousData = JsonConvert.DeserializeObject<OrderFormViewModel>(TempData["OrderFormData"].ToString());
-                model.ShippingAddress = previousData.ShippingAddress;
-                model.OrderDetails = previousData.OrderDetails;
-                model.PaymentMethodId = previousData.PaymentMethodId;
-            }
             return View(model);
         }
 
-        //Get user input data for further usage
+        //Retrieve user input (shipping info) and redirect based on payment method
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> ShippingDetails(OrderFormViewModel formModel)
@@ -59,55 +50,130 @@ namespace FlowerStore.Controllers
                 formModel.PaymentMethods = await orderService.GetAllPaymentMethodsAsync();
                 return View(formModel);
             }
-            return RedirectToAction(nameof(Preview), formModel);
+
+            TempData["OrderFormData"] = JsonConvert.SerializeObject(formModel); //save for Preview()
+            var paymentChosen = await orderService.GetChosenPaymentMethodAsync(formModel.PaymentMethodId);
+
+            if (paymentChosen.Name == "Card")
+            {
+                return RedirectToAction(nameof(CardDetails));
+            }
+            return RedirectToAction(nameof(Preview));
         }
 
-        //Get detailed information of order and create viewModel
+        //Display form for card-chosen payment
         [HttpGet]
-        public async Task<IActionResult> Preview(OrderFormViewModel formModel)
+        public async Task<IActionResult> CardDetails()
         {
-            if (!ModelState.IsValid)
+            if (await cartService.IsShoppingCartEmpty(User.GetUserId()))
             {
                 return BadRequest();
             }
 
-            var cart = await cartService.GetOrCreateShoppingCartAsync(User.GetUserId());
-            var orderModel = await orderService.CreateOrderViewModel(formModel, cart);
+            var model = new CardDetailsAddViewModel
+            {
+                UserId = User.GetUserId()
+            };
+            return View(model);
+        }
+
+        //Retrieve user's card details and redirect to Preview
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CardDetails(CardDetailsAddViewModel cardModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cardModel);
+            }
+           
+            TempData["CardDetailsData"] = JsonConvert.SerializeObject(cardModel); //save for Preview()
+            return RedirectToAction(nameof(Preview));
+        }
+
+        //Show collected order's data and create new detailed viewModel (before placing new order)
+        [HttpGet]
+        public async Task<IActionResult> Preview()
+        {
+            if (await cartService.IsShoppingCartEmpty(User.GetUserId()))
+            {
+                return BadRequest();
+            }
+
+            var formModel = JsonConvert.DeserializeObject<OrderFormViewModel>(TempData["OrderFormData"].ToString());
+
+            if (formModel == null)
+            {
+                return BadRequest();
+            }
+
+            var cart = await cartService.GetShoppingCartAsync(User.GetUserId());
+            var orderModel = await orderService.CreateOrderViewModelAsync(formModel, cart); 
 
             if (orderModel == null)
             {
                 return BadRequest();
             }
 
-            //Saving temporary data if user decides to edit the form
-            TempData["OrderFormData"] = JsonConvert.SerializeObject(formModel);
+            if (TempData.ContainsKey("CardDetailsData"))
+            {   
+                var cardModel = JsonConvert.DeserializeObject<CardDetailsAddViewModel>(TempData["CardDetailsData"].ToString());
+                orderModel.CardDetails = cardModel;                 
+            }
+
+            TempData["OrderViewModel"] = JsonConvert.SerializeObject(orderModel);
             return View(orderModel);
         }
 
-        //Create new order
+        //Create new actual order in database (and eventual CardDetails)
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> PlaceOrder(OrderViewModel model)
+        public async Task<IActionResult> PlaceOrder()
         {
-            //clear tempData
+            var orderModel = JsonConvert.DeserializeObject<OrderViewModel>(TempData["OrderViewModel"].ToString());
 
-            if (!ModelState.IsValid)
+            if (orderModel == null || !ModelState.IsValid)
             {
-                model.PaymentMethods = await orderService.GetAllPaymentMethodsAsync();
-                model.OrderStatuses = await orderService.GetAllOrderStatusesAsync();
-                return View(nameof(Preview), model);
+                return View(nameof(Preview));
             }
 
-            var newOrderId = await orderService.CreateOrderAsync(model);
+            int? cardId = null;
+            if (orderModel.CardDetails != null)
+            {
+                 cardId = await orderService.CreateCardDetailsAsync(orderModel.CardDetails);
+            }
+
+            var newOrderId = await orderService.CreateOrderAsync(orderModel, cardId);
 
             if (newOrderId == 0)
             {
                 ModelState.AddModelError("", "There was an issue creating your order. Please try again.");
-                return View(nameof(Preview), model);
+                return View(nameof(Preview));
             }
 
+            TempData.Clear();
             await cartService.ClearCartAsync(User.GetUserId());
-            return RedirectToAction("OrderConfirmation", new { id = newOrderId });
+            return RedirectToAction("Confirmed", new { orderId = newOrderId });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirmed(int orderId)
+        {
+            var order = await orderService.OrderByIdExistAsync(orderId);
+            //TODO: change orderStatus
+
+            if (order == null)
+            {
+                return NotFound("Order not found.");
+            }
+
+            if (order.UserId != User.GetUserId())
+            {
+                return Unauthorized();
+            }
+
+            ViewBag.OrderId = orderId;
+            return View();
         }
     }
 }

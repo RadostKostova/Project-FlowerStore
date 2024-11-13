91cab71aedbc708ee8d666129421fc0462916cf6
Radost Kostova <radostt_kostova@abv.bg>
2024-11-13 22:37:03 +0200

    Added missing UserId prop to CartViewModel

--- FlowerStore.Core/ViewModels/Cart/CartViewModel.cs
+++ FlowerStore.Core/ViewModels/Cart/CartViewModel.cs
@@ -5,15 +5,13 @@ namespace FlowerStore.Core.ViewModels.Cart
     /// <summary>
     /// This View Model represents the Shopping Cart.
     /// </summary>
-    
+
     public class CartViewModel
     {
         public int Id { get; set; }
-
+        public string UserId { get; set; } = string.Empty;
         public int ProductsCounter { get; set; }
-
         public decimal TotalPrice { get; set; }
-
         public ICollection<CartProductViewModel> ShoppingCartProducts { get; set; } = new List<CartProductViewModel>();
     }
 }

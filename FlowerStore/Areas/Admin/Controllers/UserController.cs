a991f4c1d36b5cd2a2e4a3570f3259403d04e190
Radost Kostova <radostt_kostova@abv.bg>
2024-11-18 17:52:28 +0200

    Created UserController with view that displays users

---
+++ FlowerStore/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using FlowerStore.Core.Contracts;
+using FlowerStore.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerStore.Areas.Admin.Controllers
+{
+    public class UserController : AdminBaseController
+    {
+
+        private readonly IAdminService adminService;
+
+        public UserController(IAdminService _adminService)
+        {
+            adminService = _adminService;
+        }
+
+        //Get all users
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var users = await adminService.GetAllUsersAsync();
+            return View(users);
+        }
+    }
+}

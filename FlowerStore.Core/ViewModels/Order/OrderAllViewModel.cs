29c8b9a1579fd629542e73cd4e2412e1e2c40b9f
Radost Kostova <radostt_kostova@abv.bg>
2024-11-18 17:41:38 +0200

    Created OrderAllViewModel to dsplay all orders for admin area

---
+++ FlowerStore.Core/ViewModels/Order/OrderAllViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FlowerStore.Core.ViewModels.Order
+{
+    public class OrderAllViewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string OrderDate { get; set; } = string.Empty;
+        public decimal TotalPrice { get; set; }
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string? OrderDetails { get; set; }
+        public string ShippingAddress { get; set; } = string.Empty;
+        public string OrderStatus { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}

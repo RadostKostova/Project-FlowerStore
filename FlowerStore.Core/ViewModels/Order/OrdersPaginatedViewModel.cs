2779189d206f79cb0667e27cb9507b6b3fbfbe45
Radost Kostova <radostt_kostova@abv.bg>
2024-11-22 01:50:38 +0200

    Created viewModel for orders pagination

---
+++ FlowerStore.Core/ViewModels/Order/OrdersPaginatedViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowerStore.Core.ViewModels.Order
+{
+    /// <summary>
+    /// ViewModel for paginated orders.
+    /// </summary>
+    public class OrdersPaginatedViewModel
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<OrderAllViewModel> Orders { get; set; } = new List<OrderAllViewModel>();
+    }
+}

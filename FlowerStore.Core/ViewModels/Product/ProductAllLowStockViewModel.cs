ddfbd167979798c7bcf20afa2b36a538d5605967
Radost Kostova <radostt_kostova@abv.bg>
2024-11-21 14:51:06 +0200

    ProductAllLowStockViewModel created for displaying low-stock products

---
+++ FlowerStore.Core/ViewModels/Product/ProductAllLowStockViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FlowerStore.Core.ViewModels.Product
+{
+    /// <summary>
+    /// This viewModel displays low-stock products.
+    /// </summary>
+    public class ProductAllLowStockViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public int FlowersCount { get; set; }
+    }
+}

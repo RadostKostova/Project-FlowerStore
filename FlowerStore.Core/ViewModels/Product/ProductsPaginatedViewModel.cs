1dd45f607041061d44fc8ae3b9114f2d39bafdeb
Radost Kostova <radostt_kostova@abv.bg>
2024-11-21 19:28:41 +0200

    Pagination viewModel for products created

---
+++ FlowerStore.Core/ViewModels/Product/ProductsPaginatedViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowerStore.Core.ViewModels.Product
+{
+    /// <summary>
+    /// ViewModel for product pagination
+    /// </summary>
+    public class ProductsPaginatedViewModel
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<ProductAllViewModel> Products { get; set; } = new List<ProductAllViewModel>();
+    }
+}

bce4bea6ede87426c7e5a21ac49013615561a39b
Radost Kostova <radostt_kostova@abv.bg>
2024-11-27 18:06:50 +0200

    Added SortOrder prop to ProductsPaginatedViewModel for future sorting

--- FlowerStore.Core/ViewModels/Product/ProductsPaginatedViewModel.cs
+++ FlowerStore.Core/ViewModels/Product/ProductsPaginatedViewModel.cs
@@ -1,12 +1,13 @@
 ﻿namespace FlowerStore.Core.ViewModels.Product
 {
     /// <summary>
-    /// ViewModel for product pagination
+    /// ViewModel for product pagination and sorting
     /// </summary>
     public class ProductsPaginatedViewModel
     {
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
         public IEnumerable<ProductAllViewModel> Products { get; set; } = new List<ProductAllViewModel>();
+        public string SortOrder { get; set; } = string.Empty;
     }
 }

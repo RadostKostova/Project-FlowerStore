45d6ee3b4f947689f951b959b6e16b520b87c0cd
Radost Kostova <radostt_kostova@abv.bg>
2024-11-28 16:17:01 +0200

    Included the ReviewService in the service collection

--- FlowerStore/Extensions/ServiceCollectionExtensions.cs
+++ FlowerStore/Extensions/ServiceCollectionExtensions.cs
@@ -22,6 +22,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IAdminService, AdminService>();
+            services.AddScoped<IReviewService, ReviewService>();
 
             return services;
         }

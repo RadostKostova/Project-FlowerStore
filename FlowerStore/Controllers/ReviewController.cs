ce7ce4eab787219c16500b708592069c5ca3705d
Radost Kostova <radostt_kostova@abv.bg>
2024-11-28 16:17:58 +0200

    Created Review controller and All view

---
+++ FlowerStore/Controllers/ReviewController.cs
@@ -0,0 +1,35 @@
+﻿using FlowerStore.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerStore.Controllers
+{
+    /// <summary>
+    /// This controller handles operation related to user reviews. Only authenticated users will be able to post a review.
+    /// </summary>
+    
+    public class ReviewController : BaseController
+    {
+        private readonly IReviewService reviewService;
+
+        public ReviewController(IReviewService _reviewService)
+        {
+            reviewService = _reviewService;
+        }
+
+        //Show all reviews
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            var reviews = await reviewService.GetAllReviewsAsync();
+            return View();
+        }
+
+        //Add review
+        public async Task<IActionResult> Add()
+        {
+            //var reviews = await reviewService.GetAllReviewsAsync();
+            return View();
+        }
+    }
+}

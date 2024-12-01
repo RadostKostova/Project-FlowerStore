c1b2fb7cddf3a743c82cb96012ba09466c34bf23
Radost Kostova <radostt_kostova@abv.bg>
2024-12-01 13:37:00 +0200

    Created viewModel for editing review

---
+++ FlowerStore.Core/ViewModels/Review/ReviewEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static FlowerStore.Infrastructure.Constants.DataConstants;
+using static FlowerStore.Infrastructure.Constants.ErrorConstants;
+
+namespace FlowerStore.Core.ViewModels.Review
+{
+    /// <summary>
+    /// ViewModel for editing review.
+    /// </summary>
+    public class ReviewEditViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(ReviewContentMaxLength,
+            MinimumLength = ReviewContentMinLength,
+            ErrorMessage = StringLengthErrorMessage)]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+    }
+}

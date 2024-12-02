432a0d9426936220c103518d9774b9f7c8ed2428
Radost Kostova <radostt_kostova@abv.bg>
2024-12-02 15:17:01 +0200

    Created new Review configuration

---
+++ FlowerStore.Infrastructure/Data/Seed/EntitiesConfiguration/ReviewConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using FlowerStore.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlowerStore.Infrastructure.Data.Seed.EntitiesConfiguration
+{
+    internal class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        /// <summary>
+        /// Seeding configuration for reviews
+        /// </summary>
+
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            var data = new DataSeed();
+
+            builder.HasData(new Review[] { data.FirstReview, data.SecondReview, data.ThirdReview });
+        }
+    }
+}

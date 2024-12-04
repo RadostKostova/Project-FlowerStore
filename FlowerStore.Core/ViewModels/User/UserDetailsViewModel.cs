51c3a0577601bf6d1836edeabff763fe3174a105
Radost Kostova <radostt_kostova@abv.bg>
2024-12-04 20:12:42 +0200

    ViewModel for user details created

---
+++ FlowerStore.Core/ViewModels/User/UserDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FlowerStore.Core.ViewModels.User
+{
+    public class UserDetailsViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public IList<string> Roles { get; set; } = new List<string>();
+    }
+}

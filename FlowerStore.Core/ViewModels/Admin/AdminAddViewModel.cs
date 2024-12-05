ccf7d5dab81528158f6439a3e46c9d08a1c50fa2
Radost Kostova <radostt_kostova@abv.bg>
2024-12-05 03:24:14 +0200

    ViewModel for adding admin and view created

---
+++ FlowerStore.Core/ViewModels/Admin/AdminAddViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlowerStore.Core.ViewModels.Admin
+{
+    public class AdminAddViewModel
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}

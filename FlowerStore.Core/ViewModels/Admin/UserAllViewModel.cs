bf24ceb6d9c656ee331a739411a4f473d74929a2
Radost Kostova <radostt_kostova@abv.bg>
2024-12-05 23:43:05 +0200

    Included additional bool prop IsAdmin

--- FlowerStore.Core/ViewModels/Admin/UserAllViewModel.cs
+++ FlowerStore.Core/ViewModels/Admin/UserAllViewModel.cs
@@ -8,5 +8,6 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
+        public bool IsAdmin { get; set; }
     }
 }

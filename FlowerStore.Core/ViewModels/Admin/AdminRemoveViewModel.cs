1d2919a6a99f926b7e0e7417237fa70761dc9ca0
Radost Kostova <radostt_kostova@abv.bg>
2024-12-05 11:38:56 +0200

    Created viewModel and view related to removing an admin

---
+++ FlowerStore.Core/ViewModels/Admin/AdminRemoveViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FlowerStore.Core.ViewModels.Admin
+{
+    public class AdminRemoveViewModel
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
